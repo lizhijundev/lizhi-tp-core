03ef2f457b12d566bbb156d763dd3bb0b4788366
lizhijun <i@lizhijun.me>
2023-12-05 04:32:27 +0800

    接管tp Request

--- src/Service/LizhiCoreService.php
+++ src/Service/LizhiCoreService.php
@@ -3,12 +3,14 @@
 namespace Lizhijun\LizhiTpCore\Service;
 
 use think\Service as BaseService;
+use Lizhijun\LizhiTpCore\Request;
 use Lizhijun\LizhiTpCore\ExceptionHandle;
 
 class LizhiCoreService extends BaseService
 {
     public function register()
     {
+        $this->app->bind('think\Request', Request::class);
         $this->app->bind('think\exception\Handle', ExceptionHandle::class);
     }
 
